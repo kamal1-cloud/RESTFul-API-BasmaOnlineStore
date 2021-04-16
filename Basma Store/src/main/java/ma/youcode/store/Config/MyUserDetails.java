b5c5e025345df186258aaeed2ccf5b2a7e1f93f7
kamal habrich <kamalhabrich50@gmail.com>
2021-04-16 07:32:22 -0300

    add spring security configuiration

---
+++ Basma Store/src/main/java/ma/youcode/store/Config/MyUserDetails.java
@@ -0,0 +1,68 @@
+package ma.youcode.store.Config;
+
+import ma.youcode.store.Modeles.Roles;
+import ma.youcode.store.Modeles.Users;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import javax.management.relation.Role;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+import java.util.Set;
+
+public class MyUserDetails implements UserDetails {
+
+    private Users user;
+
+    public MyUserDetails(Users user) {
+        this.user = user;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        Set<Roles> roles = user.getRoles();
+        List<SimpleGrantedAuthority> authorities = new ArrayList<>();
+
+        for (Roles role : roles) {
+            authorities.add(new SimpleGrantedAuthority(role.getNom()));
+        }
+
+        return authorities;
+    }
+
+    @Override
+    public String getPassword() {
+        return user.getPassword();
+    }
+
+    @Override
+    public String getUsername() {
+        return user.getEmail();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        if(this.getStatus() == false) return false;
+        return true;
+    }
+    public Boolean getStatus(){
+        return user.getStatus();
+    }
+}
