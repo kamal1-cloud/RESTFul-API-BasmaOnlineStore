3e39a7f4dd716ded95384c403a5bb38efcd43009
kamal habrich <kamalhabrich50@gmail.com>
2021-04-19 02:41:32 -0300

    Add Products microServices

---
+++ Product-Microservice/src/test/java/ma/youcode/store/ProductMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package ma.youcode.store;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductMicroserviceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
