e04eb61a7c26f4b156ecdea8f64a9be90f359101
kamal habrich <kamalhabrich50@gmail.com>
2021-04-19 02:39:34 -0300

    add jwt

--- Product-Microservice/src/main/java/ma/youcode/store/Repository/ProductRepository.java
+++
@@ -1,7 +0,0 @@
-package ma.youcode.store.Repository;
-
-import ma.youcode.store.Model.Products;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProductRepository extends JpaRepository<Products,Long> {
-}
