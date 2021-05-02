bbe72e7850627b6e83134428be1b8f3309bc3519
kamal habrich <kamalhabrich50@gmail.com>
2021-05-01 22:55:48 -0300

    Adding the Api gateway

---
+++ Api-Geteway/src/test/java/ma/youcode/apigeteway/ApiGetewayApplicationTests.java
@@ -0,0 +1,13 @@
+package ma.youcode.apigeteway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGetewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
