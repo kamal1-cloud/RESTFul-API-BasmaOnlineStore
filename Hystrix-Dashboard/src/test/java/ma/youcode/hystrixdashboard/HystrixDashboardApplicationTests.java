87297cecdcc7caff668021d5167816dbd5f95bfa
kamal habrich <kamalhabrich50@gmail.com>
2021-05-02 01:41:30 -0300

    Adding Hystrix Dashboard

---
+++ Hystrix-Dashboard/src/test/java/ma/youcode/hystrixdashboard/HystrixDashboardApplicationTests.java
@@ -0,0 +1,13 @@
+package ma.youcode.hystrixdashboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HystrixDashboardApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
