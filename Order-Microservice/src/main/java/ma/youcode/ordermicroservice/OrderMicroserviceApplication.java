c403fe05bf20e8524b869633164ffdc672653a7f
Anassifi <abdelhakimnassifi@gmail.com>
2021-05-11 18:38:24 +0000

    Eureka configuration and registry

--- Order-Microservice/src/main/java/ma/youcode/ordermicroservice/OrderMicroserviceApplication.java
+++ Order-Microservice/src/main/java/ma/youcode/ordermicroservice/OrderMicroserviceApplication.java
@@ -2,12 +2,13 @@ package ma.youcode.ordermicroservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
 
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
-//@EnableEurekaClient
+@EnableEurekaClient
 public class OrderMicroserviceApplication {
 
     public static void main(String[] args) {
