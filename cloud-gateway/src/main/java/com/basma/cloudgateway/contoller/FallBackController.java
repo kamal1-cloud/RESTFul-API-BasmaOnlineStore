5da8e050a91749a80f62ebdca920fa03ce6c389d
Anassifi <abdelhakimnassifi@gmail.com>
2021-05-22 14:07:15 +0100

    Ajout de hystrix

---
+++ cloud-gateway/src/main/java/com/basma/cloudgateway/contoller/FallBackController.java
@@ -0,0 +1,19 @@
+package com.basma.cloudgateway.contoller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Mono;
+
+@RestController
+public class FallBackController {
+
+    @RequestMapping("/orderFallBack")
+    public Mono<String> orderServiceFallback(){
+        return Mono.just("Order service is taking too long to respond or It's down. Please try later");
+    }
+
+    @RequestMapping("/paymentFallBack")
+    public Mono<String> paymentServiceFallback(){
+        return Mono.just("Payment service is taking too long to respond or It's down. Please try later");
+    }
+}
